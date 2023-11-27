96d35022d8cfe1c094a67c52742da9eff85e8518
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2023-11-27 09:20:35 +0100

    fixed indentation error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # a manifest that kills a process named killmenow.
 
 exec {  'killmenow':
-  command  => 'pkill -f killmenow',
-  path     => ['/bin:/usr/bin'],
-  onlyif   => 'pgrep -f killmenow',
+  command => 'pkill -f killmenow',
+  path    => ['/bin:/usr/bin'],
+  onlyif  => 'pgrep -f killmenow',
 }
